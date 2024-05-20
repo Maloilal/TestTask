e27d298e250a665ab753176eace74b9420cb338f
Maloilal <shaman24995@gmail.com>
2024-05-20 22:08:46 +0300

    Add styled components and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { styled } from "styled-components";
 
-function App() {
+export default function App() {
+  const Button = styled.button<{ $primary?: boolean }>`
+    /* Adapt the colors based on primary prop */
+    background: ${(props) => (props.$primary ? "#BF4F74" : "white")};
+    color: ${(props) => (props.$primary ? "white" : "#BF4F74")};
+
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #bf4f74;
+    border-radius: 3px;
+  `;
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
-
-export default App;
