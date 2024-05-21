5a955355d4633d7707d4a4fd25711bce6a690e83
Maloilal <shaman24995@gmail.com>
2024-05-21 19:33:41 +0300

    Add dropdown component

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Form, useForm } from "react-hook-form";
+import { styled } from "styled-components";
+
+const InputBox = styled.div<{ $isActive?: boolean }>`
+  width: 490px;
+  height: 45px;
+  background: white;
+  padding: 0px 12px 0px 12px;
+  gap: 10px;
+  border-radius: 6px;
+  box-shadow: none;
+  line-height: 45px;
+`;
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+const DropdownList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  position: absolute;
+  top: 55px;
+`;
+
+interface DropDownProps {
+  options: string[];
+  value: string;
+  onChange: (option: string) => void;
+}
+
+export default function DropDown(props: DropDownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <DropdownContainer>
+      <InputBox
+        $isActive={isOpen}
+        onClick={() => setIsOpen(!isOpen)}
+        style={isOpen ? { border: "2px solid green" } : { border: "none" }}
+      >
+        {props.value}
+      </InputBox>
+      {isOpen && (
+        <DropdownList>
+          {props.options.map((option, key) => {
+            return (
+              <InputBox
+                onClick={() => {
+                  props.onChange(option);
+                  setIsOpen(false);
+                }}
+                key={key}
+              >
+                {option}
+              </InputBox>
+            );
+          })}
+        </DropdownList>
+      )}
+    </DropdownContainer>
+  );
+}
--- src/components/InputComponents.tsx
+++ src/components/InputComponents.tsx
@@ -1,52 +1,82 @@
-import { Form, useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { styled } from "styled-components";
+import { useMask } from "@react-input/mask";
+import DropDown from "./DropDown";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 540px;
+  height: 850px;
+  border-radius: 12px;
+  padding: 12px, 24px, 24px, 24px;
+  gap: 21px;
+  background: #eff2f5;
+`;
+const Div = styled.div`
+  width: 490px;
+  height: 66px;
+  top: 20px;
+  left: 20px;
+  border-radius: 2px;
+  gap: 4px;
+`;
+const InputBox = styled.div`
+  width: 490px;
+  height: 44px;
+  padding: 0px 12px 0px 12px;
+  gap: 10px;
+  border-radius: 6px;
+  border-color: white;
+  outline: none;
+  border: none;
+  &:focus {
+    border: 1px solid green;
+  }
+  box-shadow: none;
+`;
+const Input = styled.input`
+  width: 490px;
+  height: 44px;
+  padding: 0px 12px 0px 12px;
+  gap: 10px;
+  border-radius: 6px;
+  border-color: white;
+  outline: none;
+  border: none;
+  &:focus {
+    border: 1px solid green;
+  }
+  box-shadow: none;
+`;
+const Label = styled.label`
+  width: 490px;
+  height: 18px;
+  font-size: 14px;
+  color: #9394aa;
+  font-weight: bold;
+`;
+const Checkbox = styled.input`
+  width: 16px;
+  height: 16px;
+  border-radius: 4px;
+  border: 1.5 solid #9394aa;
+`;
 
 export default function InputComponents() {
-  const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-    width: 540px;
-    height: 850px;
-    border-radius: 12px;
-    padding: 12px, 24px, 24px, 24px;
-    gap: 21px;
-    background: #eff2f5;
-  `;
-  const Div = styled.div`
-    width: 490px;
-    height: 66px;
-    top: 20px;
-    left: 20px;
-    border-radius: 2px;
-    gap: 4px;
-  `;
-  const Input = styled.input`
-    width: 490px;
-    height: 44px;
-    padding: 0px 12px 0px 12px;
-    gap: 10px;
-    border-radius: 6px;
-    border-color: white;
-    outline: none;
-    border: none;
-    &:focus {
-      border: 1px solid green;
-    }
-    box-shadow: none;
-  `;
-  const Label = styled.label`
-    width: 490px;
-    height: 18px;
-    font-size: 14px;
-    color: #9394aa;
-  `;
   const {
+    control,
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
   const onSubmit = (data: any) => console.log(data);
+  const inputRef = useMask({
+    mask: "___ ___.__",
+    replacement: { _: /\d/ },
+  });
+
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <div>
@@ -67,6 +97,43 @@ export default function InputComponents() {
         />
         {errors.rating && <div>Это поле обязательно</div>}
       </div>
+      <div>
+        <Checkbox
+          id="acreditation"
+          type="checkbox"
+          {...register("acreditation")}
+        />
+        <label htmlFor="acreditation">Имеется аккредитация</label>
+      </div>
+      <div>
+        <Label>Желаемая сумма</Label>
+        <Input
+          {...register("amount", { required: true })}
+          ref={inputRef}
+          placeholder="0"
+        ></Input>
+        {errors.amount && <div>Это поле обязательно</div>}
+      </div>
+
+      <Controller
+        name="select"
+        defaultValue={"Категория 1"}
+        control={control}
+        render={({ field }) => {
+          return (
+            <DropDown
+              options={[
+                "Категория 1",
+                "Категория 2",
+                "Категория 3",
+                "Категория 4",
+              ]}
+              {...field}
+            />
+          );
+        }}
+      />
+
       <button type="submit">Submit</button>
     </Form>
   );
